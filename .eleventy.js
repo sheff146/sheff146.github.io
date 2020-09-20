11787bb8f58ab6bd93bea563f63dd843c39c07e1
sheff146 <alexandre.shevchenko@gmail.com>
2020-09-20 17:03:40 +0200

    Add empty styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
-module.exports = {
-  dir: {
-    input: "src",
-    output: "dist",
+module.exports = (config) => {
+  // config.addPassthroughCopy('src/favicon.ico');
+  config.addPassthroughCopy('src/styles');
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    },
     templateFormats: ["liquid"]
   }
 };
